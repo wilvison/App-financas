92b470241eeaca6cfe2e0a8eacb1bc6cb46947c2
Wilvison Ralis <wilvison.c@hotmail.com>
2025-03-04 00:45:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configurações do Google Sheets API
 const CONFIG = {
-    CLIENT_ID: '949830020354-9lrrn42760jue4j04kfai3bk09p4upkd.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyB86hsVeUrzwQYwzbaRCYdU3paEj736MG8',
-    SPREADSHEET_ID: '1_Gstx0m4xoNA5ynVTjMoUE33vB63JqOueXh5PLkLMS4',
+    CLIENT_ID: 'SUA_CLIENT_ID',
+    API_KEY: 'SEU_API_KEY',
+    SPREADSHEET_ID: 'SEU_SPREADSHEET_ID',
     DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
     SCOPES: "https://www.googleapis.com/auth/spreadsheets",
     RANGE: 'Sheet1!A:E'
